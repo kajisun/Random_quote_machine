720d83f4d3fddabc256ed15b097e6c3fe5d90365
kajisun <papapahatena@yahoo.co.jp>
2020-08-24 18:30:59 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'typeface-roboto';
+import QuoteMachine from './components/QuoteMachine';
+import { Grid, withStyles } from '@material-ui/core';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+    container: {
+        alignItems: "center",
+        display: "flex",
+        height: "100vh",
+    }
 }
 
-export default App;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            quotes: [],
+            selectedQuoteIndex: null,
+        }
+        this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this);
+        this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+            .then(data => data.json())
+            .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
+    }
+
+    get selectedQuote() {
+        if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+            return undefined;
+        }
+        return this.state.quotes[this.state.selectedQuoteIndex];
+    }
+
+    generateNewQuoteIndex() {
+        const len = this.state.quotes.length;
+        return len ? Math.floor(Math.random() * len) : undefined;
+    }
+
+    assignNewQuoteIndex() {
+        this.setState({selectedQuoteIndex: this.generateNewQuoteIndex()})
+    }
+    
+    nextQuoteClickHandler() {
+        alert("hi")
+        console.log("hi")
+    }
+
+    render() {  
+        console.log(this.state.quotes[1])
+        return (
+            <Grid className={this.props.classes.container} id="quote-box" justify="center" container>
+                <Grid xs={11} lg={8} item>
+                    {
+                        this.selectedQuote ?
+                            <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={ this.assignNewQuoteIndex } />
+                         : null
+                    }
+                </Grid>
+            </Grid>
+        );
+    }
+}
+
+
+export default withStyles(styles)(App);
